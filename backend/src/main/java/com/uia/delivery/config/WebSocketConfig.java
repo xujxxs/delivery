e6e036805f374334d84b01abf23d3bf11d99fcdd
x_ujxxs <jakuresi@mail.ru>
2025-05-20 03:03:00 +0400

    Added CORS configurations

--- backend/src/main/java/com/uia/delivery/config/WebSocketConfig.java
+++ backend/src/main/java/com/uia/delivery/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.uia.delivery.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,6 +11,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer 
 {
+    @Value("${app.cors.allowed-origins}")
+    private String[] allowedOrigins;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) 
     {
@@ -20,6 +24,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) 
     {
-        registry.addEndpoint("/v3/connect").withSockJS();
+        registry.addEndpoint("/v3/connect")
+            .setAllowedOrigins(allowedOrigins)
+            .withSockJS();
     }
 }
